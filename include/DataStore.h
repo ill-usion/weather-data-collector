df9d1846c082407876ab72347b2baca885abc632
ill-usion <aalhadrami7@gmail.com>
2026-07-27 11:00:14 +0400

    open files on demand

--- include/DataStore.h
+++ include/DataStore.h
@@ -13,7 +13,7 @@ enum class OverflowBehavior
 class DataStore
 {
 private:
-    File m_file;
+    String m_filename;
     OverflowBehavior m_overflowBehavior;
 
 public:
@@ -22,10 +22,9 @@ public:
     /// @brief Constructs a new DataStore object with the given file
     /// @param file DataStore file
     /// @param overflowBehavior Overflow behavior
-    DataStore(File file, OverflowBehavior overflowBehavior = OverflowBehavior::Overwrite) : m_file(file), m_overflowBehavior(overflowBehavior) {}
+    DataStore(String filename, OverflowBehavior overflowBehavior = OverflowBehavior::Overwrite) : m_filename(filename), m_overflowBehavior(overflowBehavior) {}
     ~DataStore()
     {
-        m_file.close();
     }
 
     /// @brief Opens a data store
@@ -34,13 +33,16 @@ public:
     /// @return A new instance of DataStore
     static DataStore open(const char *filename, OverflowBehavior overflowBehavior = OverflowBehavior::Overwrite)
     {
-        return DataStore(LittleFS.open(filename, "a", true));
+        return DataStore(String(filename), overflowBehavior);
     }
 
     /// @brief Clears the data store
     void clear()
     {
-        LittleFS.remove(m_file.path());
+        if (LittleFS.exists(m_filename))
+            LittleFS.remove(m_filename);
+        if (LittleFS.exists(m_filename + ".idx"))
+            LittleFS.remove(m_filename + ".idx");
     }
 
     /// @brief Appends the given data to the data store
@@ -49,32 +51,92 @@ public:
     /// @return Number of bytes written
     size_t store(uint8_t *data, size_t len)
     {
+        int seekPos = -1;
+
+        // Serial.printf("Total bytes: %d, Used bytes: %d, Free bytes: %d\n", LittleFS.totalBytes(), LittleFS.usedBytes(), LittleFS.totalBytes() - LittleFS.usedBytes());
+        // Serial.printf("Data length: %d\n", len);
         if (len > LittleFS.totalBytes() - LittleFS.usedBytes())
         {
             if (m_overflowBehavior == OverflowBehavior::Clear)
             {
                 clear();
-                m_file = LittleFS.open(m_file.path(), "a", true);
             }
             else if (m_overflowBehavior == OverflowBehavior::Overwrite)
             {
-                m_file.seek(0);
+                seekPos = getOverwriteIndex();
+                // Serial.printf("Seek position: %d\n", seekPos);
             }
         }
-        return m_file.write(data, len);
+
+        File writer = getWriter();
+        if (!writer)
+        {
+            // Serial.println("Failed to write to data store. Writer is not valid.");
+            return -1;
+        }
+
+        if (seekPos != -1)
+            writer.seek(seekPos);
+
+        size_t written = writer.write(data, len);
+        writer.close();
+
+        if (written > 0 && m_overflowBehavior == OverflowBehavior::Overwrite)
+        {
+            size_t sz = this->size();
+            setOverwriteIdx((seekPos + written) % sz);
+        }
+
+        return written;
     }
 
     /// @brief Size of the data store
     /// @return Size in bytes
     size_t size()
     {
-        return m_file.size();
+        if (!LittleFS.exists(m_filename))
+            return 0;
+
+        File reader = getReader();
+        size_t s = reader.size();
+        reader.close();
+        return s;
     }
 
-    /// @brief The underlying DataStore file
-    /// @return DataStore file
-    File *getFilePtr()
+    /// @brief Gets a file reader for the data store
+    /// @return File reader
+    File getReader()
     {
-        return &m_file;
+        return LittleFS.open(m_filename, FILE_READ);
+    }
+
+    /// @brief Gets a file writer for the data store
+    /// @return File writer
+    File getWriter()
+    {
+        return LittleFS.open(m_filename, FILE_WRITE, true);
+    }
+
+private:
+    uint32_t getOverwriteIndex()
+    {
+        String fileMetaPath = m_filename + ".idx";
+        if (!LittleFS.exists(fileMetaPath))
+            return 0;
+
+        File fileMeta = LittleFS.open(fileMetaPath, FILE_READ);
+        String content = fileMeta.readString();
+        fileMeta.close();
+        uint32_t idx = content.toInt();
+        return idx;
+    }
+
+    size_t setOverwriteIdx(uint32_t idx)
+    {
+        String fileMetaPath = m_filename + ".idx";
+        File fileMeta = LittleFS.open(fileMetaPath, FILE_WRITE, true);
+        size_t s = fileMeta.print(idx);
+        fileMeta.close();
+        return s;
     }
 };
